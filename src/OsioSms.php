59a570a040108255a5c3723536fc0c23bc45eb4b
Nicolas <nicolas.baud12@gmail.com>
2024-08-12 17:15:33 +0200

    Update OsioSms.php

--- src/OsioSms.php
+++ src/OsioSms.php
@@ -4,12 +4,13 @@ namespace OsioSms;
 
 use OsioSms\Api\Contacts;
 use OsioSms\Api\Groups;
-/*use OsioSms\Api\SmsApi;
-use OsioSms\Api\VoiceApi;
-use OsioSms\Api\MmsApi;
-use OsioSms\Api\WhatsappApi;
-use OsioSms\Api\OtpApi;
-use OsioSms\Api\ViberApi;*/
+use OsioSms\Api\Sms;
+use OsioSms\Api\Voice;
+use OsioSms\Api\Mms;
+use OsioSms\Api\Whatsapp;
+use OsioSms\Api\Otp;
+use OsioSms\Api\Viber;
+use OsioSms\Api\Profile;
 
 class OsioSms
 {
@@ -30,33 +31,38 @@ class OsioSms
         return new Groups($this->apiToken);
     }
 
-    /*public function sms()
+    public function sms()
     {
-        return new SmsApi($this->apiToken);
+        return new Sms($this->apiToken);
     }
 
     public function voice()
     {
-        return new VoiceApi($this->apiToken);
+        return new Voice($this->apiToken);
     }
 
     public function mms()
     {
-        return new MmsApi($this->apiToken);
+        return new Mms($this->apiToken);
     }
 
     public function whatsapp()
     {
-        return new WhatsappApi($this->apiToken);
+        return new Whatsapp($this->apiToken);
     }
 
     public function otp()
     {
-        return new OtpApi($this->apiToken);
+        return new Otp($this->apiToken);
     }
 
     public function viber()
     {
-        return new ViberApi($this->apiToken);
-    }*/
+        return new Viber($this->apiToken);
+    }
+
+    public function profile()
+    {
+        return new profile($this->apiToken);
+    }
 }
