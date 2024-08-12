a857bdec642deffffe4301a9ddb248a895f1ea87
Nicolas <nicolas.baud12@gmail.com>
2024-08-12 16:46:47 +0200

    Create Sms.php

---
+++ src/Api/Sms.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace OsioSms\Api;
+
+class Sms extends Base
+{
+    public function sendSms($recipient, $senderId, $type, $message, $scheduleTime = null, $dltTemplateId = null)
+    {
+        $url = "sms/send";
+        $data = [
+            'recipient' => $recipient,
+            'sender_id' => $senderId,
+            'type' => $type,
+            'message' => $message,
+        ];
+
+        if ($scheduleTime) {
+            $data['schedule_time'] = $scheduleTime;
+        }
+
+        if ($dltTemplateId) {
+            $data['dlt_template_id'] = $dltTemplateId;
+        }
+
+        return $this->sendRequest('POST', $url, $data);
+    }
+
+    public function sendCampaign($contactListId, $senderId, $type, $message, $scheduleTime = null, $dltTemplateId = null)
+    {
+        $url = "sms/campaign";
+        $data = [
+            'contact_list_id' => $contactListId,
+            'sender_id' => $senderId,
+            'type' => $type,
+            'message' => $message,
+        ];
+
+        if ($scheduleTime) {
+            $data['schedule_time'] = $scheduleTime;
+        }
+
+        if ($dltTemplateId) {
+            $data['dlt_template_id'] = $dltTemplateId;
+        }
+
+        return $this->sendRequest('POST', $url, $data);
+    }
+
+    public function viewSms($uid)
+    {
+        $url = "sms/{$uid}";
+
+        return $this->sendRequest('GET', $url);
+    }
+
+    public function viewAllSms()
+    {
+        $url = "sms/";
+
+        return $this->sendRequest('GET', $url);
+    }
+
+    public function viewCampaign($uid)
+    {
+        $url = "campaign/{$uid}/view";
+
+        return $this->sendRequest('GET', $url);
+    }
+}
