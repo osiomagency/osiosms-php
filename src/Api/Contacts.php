ecad072ba82cae25eb583587dc0a092a5211dc12
Nicolas <nicolas.baud12@gmail.com>
2024-08-12 15:04:57 +0200

    Create Contacts.php

---
+++ src/Api/Contacts.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace OsioSms\Api;
+
+class ContactsApi extends BaseApi
+{
+    public function createContact($groupId, $phone, $fields = [])
+    {
+        $url = "contacts/{$groupId}/store";
+        $data = array_merge(['PHONE' => $phone], $fields);
+
+        return $this->sendRequest('POST', $url, $data);
+    }
+
+    public function viewContact($groupId, $uid)
+    {
+        $url = "contacts/{$groupId}/search/{$uid}";
+
+        return $this->sendRequest('POST', $url);
+    }
+
+    public function updateContact($groupId, $uid, $phone, $fields = [])
+    {
+        $url = "contacts/{$groupId}/update/{$uid}";
+        $data = array_merge(['PHONE' => $phone], $fields);
+
+        return $this->sendRequest('PATCH', $url, $data);
+    }
+
+    public function deleteContact($groupId, $uid)
+    {
+        $url = "contacts/{$groupId}/delete/{$uid}";
+
+        return $this->sendRequest('DELETE', $url);
+    }
+
+    public function viewAllContacts($groupId)
+    {
+        $url = "contacts/{$groupId}/all";
+
+        return $this->sendRequest('POST', $url);
+    }
+}
